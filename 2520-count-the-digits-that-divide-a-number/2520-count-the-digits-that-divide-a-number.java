84174203f174610ece5bdb60e96db43a879cc887
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-01-12 14:40:35 +0600

    Time: 0 ms (100.00%), Space: 39.3 MB (45.80%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
@@ -2,7 +2,7 @@ class Solution {
    public int countDigits(int num) {
         int result = 0;
         String number = String.valueOf(num);
-        if (!number.contains("0")) {
+        // if (!number.contains("0")) {
             for (int j = 0; j < number.length(); j++) {
                 int k = Character.digit(number.charAt(j), 10);
 
@@ -10,7 +10,7 @@ class Solution {
                     result++;
                 }
             }
-        }
+        // }
         return result;
     }
 }
