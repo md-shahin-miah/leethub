f0d8c47890f5f90a8381884e7a3fe084a17506d5
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-01-13 13:04:41 +0600

    Time: 0 ms (100.00%), Space: 39.4 MB (58.56%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,19 +1,12 @@
 class Solution {
   public int smallestEvenMultiple(int n) {
-   int number = 0;
-        if (n % 2 == 0 && n % n == 0) {
+        if (n % 2 == 0 ) {
             return n;
         }
-        int i = n;
-        while (number!=i){
-            if (i % 2 == 0 && i % n == 0) {
-                number = i;
-                break;
-            }
-
-            i++;
-        }
-        return number;
+      else{
+          
+          return n*2;
+      }
 
     }
 }
