795b8d84c995db03eb713a5ea11fba7c85c2ca04
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-01-21 22:31:19 +0600

    Time: 1 ms (70.52%), Space: 42.3 MB (18.65%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,35 @@
+class Solution {
+   public String[] findWords(String[] words) {
+    Map<Character, Integer> map = new HashMap<>();
+    update(map, "qwertyuiop", 1);
+    update(map, "asdfghjkl", 2);
+    update(map, "zxcvbnm", 3);
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+      int rowIdx = -1;
+      for (char c : words[i].toCharArray()) {
+        if (rowIdx == -1) {
+          rowIdx = map.get(Character.toLowerCase(c));
+        }
+        if (map.get(Character.toLowerCase(c)) != rowIdx) {
+          rowIdx = -1;
+          break;
+        }
+      }
+      if (rowIdx != -1) {
+        list.add(i);
+      }
+    }
+    String[] ans = new String[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+      ans[i] = words[list.get(i)];
+    }
+    return ans;
+  }
+  
+  private void update(Map<Character, Integer> map, String s, int row) {
+    for (char c : s.toCharArray()) {
+      map.put(c, row);
+    }
+  }
+}
