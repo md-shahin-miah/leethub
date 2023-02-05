fc79b14def60e4e58f344a9edbf7fc7f741c069f
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-02-05 19:38:09 +0600

    Time: 2 ms (88.25%), Space: 42 MB (97.84%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        StringBuilder ans = new StringBuilder();
+        for (int c = 0; i >= 0 || j >= 0 || c > 0; --i, --j) {
+            int a = i < 0 ? 0 : num1.charAt(i) - '0';
+            int b = j < 0 ? 0 : num2.charAt(j) - '0';
+            c += a + b;
+            ans.append(c % 10);
+            c /= 10;
+        }
+        return ans.reverse().toString();
+    }
+}
