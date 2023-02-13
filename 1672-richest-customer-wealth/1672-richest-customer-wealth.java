f9fc4f3aee6531e50576b5259ca08b41578baa59
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-02-13 21:39:57 +0600

    Time: 0 ms (100.00%), Space: 41.3 MB (99.39%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans = 0;
+        for (var e : accounts) {
+            int s = 0;
+            for (int v : e) {
+                s += v;
+            }
+            ans = Math.max(ans, s);
+        }
+        return ans;
+    }
+}
