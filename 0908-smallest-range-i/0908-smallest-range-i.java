03a305b64b2cb0f5627ea3502177742813d4ccf7
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-02-19 19:35:29 +0600

    Time: 2 ms (85.68%), Space: 42.6 MB (77.22%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRangeI(int[] A, int K) {
+       int min = A[0], max = A[0];
+        
+        for (int x: A) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        return Math.max(0, max - min - 2 * K);  
+    }
+}
