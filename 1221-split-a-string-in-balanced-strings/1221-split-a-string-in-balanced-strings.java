9b2cf2c88f6d9c0c707bcab0ad17c7e8f5a7e0d8
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-02-25 13:32:49 +0600

    Time: 0 ms (100.00%), Space: 40.3 MB (36.58%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int ans = 0, l = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'L') {
+                ++l;
+            } else {
+                --l;
+            }
+            if (l == 0) {
+                ++ans;
+            }
+        }
+        return ans;   
+    }
+}
