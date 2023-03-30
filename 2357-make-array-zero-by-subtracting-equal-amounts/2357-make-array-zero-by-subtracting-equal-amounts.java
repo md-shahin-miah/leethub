2d188fdfd5b1e4580eecc73d424853c1411ad99d
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-03-30 15:57:50 +0600

    Time: 0 ms (100.00%), Space: 40.5 MB (29.77%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        boolean[] s = new boolean[101];
+        
+        s[0] = true;
+        int ans = 0;
+        for (int x : nums) {
+            if (!s[x]) {
+                ++ans;
+                s[x] = true;
+            }
+        }
+        return ans;  
+    }
+}
