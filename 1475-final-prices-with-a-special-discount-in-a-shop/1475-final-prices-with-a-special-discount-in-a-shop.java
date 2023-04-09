c8099a7f2539926996c57e7cd1f0f73da28145e8
Mohammad Shahin <68494371+ShahinMohammad-insaneCoder@users.noreply.github.com>
2023-04-09 13:24:44 +0600

    Time: 1 ms (98.70%), Space: 43.1 MB (11.23%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+              
+        int n = prices.length;
+        int[] ans = new int[n];
+        
+        for (int i = 0; i < n; ++i) {
+            ans[i] = prices[i];
+            for (int j = i + 1; j < n; ++j) {
+                if (prices[j] <= prices[i]) {
+                    ans[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return ans;  
+    }
+}
